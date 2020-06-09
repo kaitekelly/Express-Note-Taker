ad2fcb537c7420a20e4accfcdb0907d3feb2c05c
Kathleen Kelly <murphy.kaite@gmail.com>
2020-06-09 13:55:42 -0700

    linked js files with require

--- Develop/server.js
+++ Develop/server.js
@@ -1,22 +1,32 @@
-const Index = require("./index.js")
 
 let express = require("express");
-
 let http = require("http");
 let fs = require("fs");
-
+let path = require("path")
 let app = express();
 let PORT = 8080;
 
-let server = http.createServer(handleRequest);
+let server = http.createServer();
 
 //Routes
+
+// api routes
+
+// api/notes with method GET
+// api/notes POST
+// api/notes/:id  method DELETE
+
+// html routes
+
+// return index.html
+//
+// route return notes.html 
 app.get("/", function(req, res) {
-    res.send("Welcome to your Home Town!!");
+    res.send("Welcome to your Home Town!!");  // return the index.html
 });
 
 app.get("/notes", function(req, res) {
-    res.json(notes);
+    res.sendfile(notes);   // return the notes.html
 })
 
 
@@ -56,3 +66,6 @@ app.get("/notes", function(req, res) {
 server.listen(PORT, function() {
     console.log("Server is listening on: http://localhost:" + PORT);
 })
+
+
+module.exports = server;
