0207378c26a72954c4d4a2adf6be4c400cf7d086
Kathleen Kelly <murphy.kaite@gmail.com>
2020-06-13 17:21:22 -0700

    deleted console logs

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,22 +1,12 @@
-// * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
-
-// * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
-
-// * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 const fs = require("fs");
 let { v4: uuidv4 } = require("uuid");
-// const util = require("util");
 
 module.exports = (app) => {
-  //app.get(path, callback [, callback ...])
   app.get("/api/notes", (req, res) => {
     fs.readFile("./db/db.json", "utf-8", (error, data) => {
       if (error) {
-        return console.log(`this is a line 14 ${error}`);
       }
       res.json(JSON.parse(data));
-      console.log(`this ${data} is from app.get. it was parsed`);
-
     });
   });
 
@@ -34,10 +24,7 @@ module.exports = (app) => {
       }
       
       let testRead = JSON.parse(noteData);
-      console.log(testRead, "this is my test read");
-      console.log(noteData);
       testRead.push(newNote);
-      console.log(`this ${noteData} is from app.get.in the readFilemethod`);
       fs.writeFile("./db/db.json", JSON.stringify(testRead, null, 2), err => {
         if (err) throw err;
         res.send(testRead);
@@ -49,7 +36,6 @@ module.exports = (app) => {
   });
 
   app.delete("/api/notes/:id", (req, res) => {
-    console.log(req.parmas);
     let noteId = req.params.id;
     fs.readFile("./db/db.json", "utf-8", (error, noteData) => {
       let testRead = JSON.parse(noteData);
@@ -57,7 +43,6 @@ module.exports = (app) => {
       fs.writeFile("./db/db.json", JSON.stringify(updatedNoteData, null, 2), err => {
         if (err) throw err;
         res.send(updatedNoteData);
-        console.log('Successfully wrote file');
       })
 
     })
@@ -65,9 +50,3 @@ module.exports = (app) => {
 
   });
 };
-
-// function renderHtml() {
-//   fs.writeFileSync("./public/assets/notes.html", render(req.body), "utf-8")
-// }
-// function readFile();
-////*********************************************** */
